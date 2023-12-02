d021a77789f1a417c1561b0817ecbbe3b172493f
benibuehler <beni.buehler@bluewin.ch>
2023-12-02 08:03:53 +0100

    fix stopwatch logic on solver

--- src/solver.rs
+++ src/solver.rs
@@ -13,7 +13,7 @@ pub fn run_solver(solver: &dyn Solver, inputs: Vec<String>) {
 
     let mut sw2 = Stopwatch::start_new();
     let result_part2 = solver.solve_part2(&inputs);
-    // sw2.stop();
+    sw2.stop();
 
     println!("Part 1: {}", result_part1);
     println!("Part 2: {}", result_part2);
@@ -21,19 +21,19 @@ pub fn run_solver(solver: &dyn Solver, inputs: Vec<String>) {
     // timing information
     println!("");
     let mut str_seconds = String::from("microseconds");
-    let p1_micro: u128 =  sw1.elapsed().as_micros();
+    let p1_micro =  sw1.elapsed().as_micros() as f64;
     let mut p1 = p1_micro;
 
-    if p1_micro  as f64 / 1000.0 > 1.0 {
-        p1 = p1_micro/1000;
+    if p1_micro / 1000.0 > 1.0 {
+        p1 = p1_micro/1000.0;
         str_seconds = String::from("milliseconds");
     }
     println!("time for part1: {p1} {str_seconds}");
 
-    let p2_micro =  sw2.elapsed().as_micros();
+    let p2_micro =  sw2.elapsed().as_micros()  as f64;
     let mut p2 = p2_micro;
-    if p2_micro as f64 / 1000.0 > 1.0 {
-        p2 = p2_micro/1000;
+    if p2_micro / 1000.0 > 1.0 {
+        p2 = p2_micro/1000.0;
         str_seconds = String::from("milliseconds");
     }
     println!("time for part2: {p2} {str_seconds}");
